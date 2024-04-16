a3ed135a382e22b0bff3f0a9b28f0ddc82cb2365
Abdulhameed Ibadi <abduh.ma96@gmail.com>
2024-04-16 22:14:08 +0300

    style the about section

--- little-lemon/src/js/about.js
+++ little-lemon/src/js/about.js
@@ -3,8 +3,13 @@ import React from 'react';
 function About() {
   return (
     <section id="about" className="about">
-      <h2>About Us</h2>
-      <p>Founded in 1990, our restaurant has been serving up delicious meals made with the freshest ingredients. Come taste the difference!</p>
+      <div className="about-content">
+        <h2>About Us</h2>
+        <p>Founded in 1990, our restaurant has been serving up delicious meals made with the freshest ingredients. Come taste the difference!</p>
+      </div>
+      <div className="about-image">
+        <img src="./about.jpg" alt="About Our Restaurant" />
+      </div>
     </section>
   );
 }
