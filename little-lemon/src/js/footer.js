b79ddb5395f0b6a5eacbdaf4f5b839fca154d172
Abdulhameed Ibadi <abduh.ma96@gmail.com>
2024-04-16 22:47:41 +0300

    finish initial css

--- little-lemon/src/js/footer.js
+++ little-lemon/src/js/footer.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-logo">Restaurant Logo</div>
+      <div className="footer-logo">
+        <img src="./logo.png" alt="Restaurant logo" />
+      </div>
       <nav className="footer-nav">
         <ul>
-          <li><a href="#main">Home</a></li>
-          <li><a href="#specials">Specials</a></li>
-          <li><a href="#testimonials">Testimonials</a></li>
-          <li><a href="#about">About</a></li>
+          <li>
+            <a href="#main">Home</a>
+          </li>
+          <li>
+            <a href="#specials">Specials</a>
+          </li>
+          <li>
+            <a href="#testimonials">Testimonials</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
         </ul>
       </nav>
+      <div className="social-media-links">
+        <a href="./#">Facebook</a> | <a href="./#">Twitter</a> |{" "}
+        <a href="./#">Instagram</a>
+      </div>
       <div className="contact-info">
         <p>123 Main Street, City, Country</p>
         <p>Phone: (123) 456-7890</p>
         <p>Email: contact@restaurant.com</p>
-      </div>
-      <div className="social-media-links">
-        <a href="#">Facebook</a> | <a href="#">Twitter</a> | <a href="#">Instagram</a>
       </div>
     </footer>
   );
